971601545f4c41ac51188e1ed0a2a8a1646ce99a
Cyril Firsov <kifirigor@gmail.com>
2024-07-14 17:24:56 +0300

    фикс

--- internal/domain/models/accrual.go
+++ internal/domain/models/accrual.go
@@ -3,5 +3,5 @@ package models
 type Accrual struct {
 	OrderNum string      `json:"order"`
 	Status   OrderStatus `json:"status"`
-	Accrual  int64       `json:"accrual"`
+	Accrual  float64     `json:"accrual"`
 }
